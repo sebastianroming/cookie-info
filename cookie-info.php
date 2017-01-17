2b7ad8562d6d8c762cad26b02baf35ec6891a4cc
Sebastian Roming <s.roming@sw-machines.de>
2017-01-17 08:37:20 +0100

    Removed ob_end_flush

--- cookie-info.php
+++ cookie-info.php
@@ -3,18 +3,18 @@
 Plugin Name: Cookie-Info
 Plugin URI: http://www.sebastianroming.de/wordpress-plugins/
 Description: Ein simples Plugin zur Konformität mit der EU Cookie Richtline
-Version: 0.1.1
+Version: 0.1.2
 Author: Sebastian Roming
 Author URI: http://www.sebastianroming.de
 License: GPL2
 Text Domain: cookie-info
 */
 
-/*	
+/*
 	Copyright 2016 Sebastian Roming
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -26,6 +26,5 @@ Text Domain: cookie-info
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-ob_end_flush();
 
 require_once dirname( __FILE__ ) . '/cookie-info-core.php';
